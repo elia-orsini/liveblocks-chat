f712e6b1dca1606d46764648ac83d3952fcb7563
Your name here <elia.orsini@hotmail.com>
2022-08-30 16:40:19 +0100

    styling

--- liveblocks.config.js
+++ liveblocks.config.js
@@ -2,8 +2,7 @@ import { createClient } from "@liveblocks/client";
 import { createRoomContext } from "@liveblocks/react";
 
 const client = createClient({
-  publicApiKey: "your-key",
+  publicApiKey: "pk_test_YaS5GvNnw88ux-wIk_B7-_yY",
 });
 
-export const { RoomProvider, useOthers, useUpdateMyPresence, useList } =
-  createRoomContext(client);
+export const { RoomProvider, useOthers, useUpdateMyPresence, useList } = createRoomContext(client);
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Chat from "./Chat";
 
 export default function App() {
-  return <Chat you="3493832" other="6423418" />;
+  return <Chat you="6423418" other="3493832" />;
 }
